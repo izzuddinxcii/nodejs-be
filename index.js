cfeb114086af1cc145673428225f32f6cf0ec912
Izzuddin Fauzi <izzuddinfz@gmail.com>
2022-12-04 07:40:38 +0800

    Middleware / Router

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const app = express()
+const router = require("./router")
 
 
 //listening on port 80
@@ -9,5 +10,8 @@ const port = 80
 
 //console log server port
 app.listen(process.env.PORT || port, () => {
-    console.log(`server up on port ${port}`);
+    console.log(`Server up on port ${port}`);
 })
+
+//middleware
+app.use(router);
